1686fc8df8f5503b50a0a79bc32eadee85433605
SyauqiAlifI <vstudio776@gmail.com>
2022-09-01 15:00:43 +0700

    otw carousel

---
+++ Codedu/home/home.js
@@ -0,0 +1,9 @@
+window.onscroll = function () {fixnav()}
+function fixnav() {
+    let home_nav = document.getElementById('main-nav')
+    if (document.documentElement.scrollTop > 1) {
+        home_nav.className = "fixed-nav"
+    } else {
+        home_nav.className = ""
+    }
+}
