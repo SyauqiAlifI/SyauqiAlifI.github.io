87f6c4ee41303b2f98e0cc40ce5949ad2d18f417
SyauqiAlifI <vstudio776@gmail.com>
2022-09-08 07:07:54 +0700

    x

--- Codedu/public/js/loadpage.js
+++
@@ -1,8 +0,0 @@
-function loadingOpen() {
-    myVar = setTimeout(showBody, 800)
-}
-function showBody() {
-    let mBody = document.querySelector("body")
-    mBody.style.display = "block"
-    mBody.className = "animate-in"
-}
